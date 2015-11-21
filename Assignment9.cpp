556bca8f98dd4c053985f032d10e72293f95b984
Michael <xenophon.of.athens@gmail.com>
2015-11-20 18:43:14 -0700

    tenth commit

--- Assignment9.cpp
+++ Assignment9.cpp
@@ -7,60 +7,9 @@
 #include <cstdlib>
 #include <dlfcn.h>
 
-std::string showHelp() { //if incorrect command is entered
-	return "--- Assign 1 Help ---\n\n-fib [n] Compute the fibonacci of [n]\n\n-e [n] Compute the value of 'e' using [n] iterations\n\n-pi [n] Compute Pi to [n] digits";
-}
-
-int userInput(char prompt[]) { //handles user input and determines calculation
-	int number = 0;
-	if (prompt[0] == 'f') { //checks if prompt is fib
-		if (prompt[1] == 'i') {
-			if (prompt[2] == 'b') {	
-				if (std::isdigit(prompt[5])) {
-					number = std::stoi(&prompt[4]) * 10 + std::stoi(&prompt[5]);
-				}
-				else {
-					number = std::stoi(&prompt[4]);
-				}
-				return fibInput(number);
-			}
-		}
-	}
-	else if (prompt[0] == 'e') { //checks if prompt is e
-		if (std::isdigit(prompt[3])) {
-			number = std::stoi(&prompt[2]) * 10 + std::stoi(&prompt[3]);
-		}
-		else {
-			number = std::stoi(&prompt[2]);
-		}
-		return eInput(number);
-	}
-	else if (prompt[0] == 'p') { //checks if prompt is pi
-		if (prompt[1] == 'i') {
-			if (std::isdigit(prompt[4])) {
-				number = std::stoi(&prompt[3]) * 10 + std::stoi(&prompt[4]);
-			}
-			else {
-				number = std::stoi(&prompt[3]);
-			}
-			return piInput(number);
-		}
-	}
-	return -1; //if input is not correct
-}
-
-bool checkQuit(prompt[]) {
-	if(prompt[0] == 'q') {
-		if (prompt[1] == 'u') {
-			if (prompt[2] == 'i') {
-				if (prompt[3] == 't') {
-					return true; //user entered 'quit'
-				}
-			}
-		}
-	}
-	return false; //user didn't enter 'quit'
-}
+std::string showHelp();
+bool checkQuit(char prompt[]);
+int userInput(char prompt[]);
 
 int main() {
 	bool done = false;
@@ -100,4 +49,62 @@ int main() {
 			std::cout << userInput(prompt) << std::endl;
 		}
 	}
+}
+
+std::string showHelp() { //if incorrect command is entered
+	return "--- Assign 1 Help ---\n\n-fib [n] Compute the fibonacci of [n]\n\n-e [n] Compute the value of 'e' using [n] iterations\n\n-pi [n] Compute Pi to [n] digits";
+}
+
+
+
+bool checkQuit(char prompt[]) {
+	if(prompt[0] == 'q') {
+		if (prompt[1] == 'u') {
+			if (prompt[2] == 'i') {
+				if (prompt[3] == 't') {
+					return true; //user entered 'quit'
+				}
+			}
+		}
+	}
+	return false; //user didn't enter 'quit'
+}
+
+
+int userInput(char prompt[]) { //handles user input and determines calculation
+	int number = 0;
+	if (prompt[0] == 'f') { //checks if prompt is fib
+		if (prompt[1] == 'i') {
+			if (prompt[2] == 'b') {	
+				if (std::isdigit(prompt[5])) {
+					number = std::stoi(&prompt[4]) * 10 + std::stoi(&prompt[5]);
+				}
+				else {
+					number = std::stoi(&prompt[4]);
+				}
+				return fibInput(number);
+			}
+		}
+	}
+	else if (prompt[0] == 'e') { //checks if prompt is e
+		if (std::isdigit(prompt[3])) {
+			number = std::stoi(&prompt[2]) * 10 + std::stoi(&prompt[3]);
+		}
+		else {
+			number = std::stoi(&prompt[2]);
+		}
+		return eInput(number);
+	}
+	else if (prompt[0] == 'p') { //checks if prompt is pi
+		if (prompt[1] == 'i') {
+			if (std::isdigit(prompt[4])) {
+				number = std::stoi(&prompt[3]) * 10 + std::stoi(&prompt[4]);
+			}
+			else {
+				number = std::stoi(&prompt[3]);
+			}
+			return piInput(number);
+		}
+	}
+	return -1; //if input is not correct
 }
