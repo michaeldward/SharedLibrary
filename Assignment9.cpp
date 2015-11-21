beac99984138ca9f16565038c483553cdd35cf4e
Michael <xenophon.of.athens@gmail.com>
2015-11-20 19:34:26 -0700

    commit 23

--- Assignment9.cpp
+++ Assignment9.cpp
@@ -12,6 +12,7 @@ std::string showHelp() { //if incorrect command is entered
 }
 
 double userInput(char prompt[]) { //handles user input and determines calculation
+	//load shared library
 	void* handle = dlopen("./library.so", RTLD_LAZY);
 	if (!handle)
 	{
@@ -43,32 +44,38 @@ double userInput(char prompt[]) { //handles user input and determines calculatio
 			if (prompt[2] == 'b') {
 				if (std::isdigit(prompt[4])) {
 					number = std::stoi(&prompt[4]);
-					return fibInput(number);
+					fibReturn = fibInput(number);
+					if (fibReturn == -1) { //number out of bounds
+						std::cout << "Please enter a number between 1 and 14.\n";
+					}
+					return fibReturn;
 				}
 			}
 		}
 	}
 	else if (prompt[0] == 'e') { //checks if prompt is e
-		if (std::isdigit(prompt[3])) {
-			number = std::stoi(&prompt[2]) * 10 + std::stoi(&prompt[3]);
-		}
-		else {
+		if (std::isdigit(prompt[2])) {
 			number = std::stoi(&prompt[2]);
+			double eReturn = eInput(number);
+			if (eReturn == -1) { //number out of bounds
+				std::cout << "Please enter a number between 1 and 14.\n";
+			}
+			return eReturn;
 		}
-		return eInput(number);
 	}
 	else if (prompt[0] == 'p') { //checks if prompt is pi
 		if (prompt[1] == 'i') {
-			if (std::isdigit(prompt[4])) {
-				number = std::stoi(&prompt[3]) * 10 + std::stoi(&prompt[4]);
-			}
-			else {
+			if (std::isdigit(prompt[3])) {
 				number = std::stoi(&prompt[3]);
-			}
-			return piInput(number);
+				piReturn = piInput(number);
+				if (piReturn == -1) { //number out of bounds
+					std::cout << "Please enter a number between 1 and 14.\n";
+				}
+				return piReturn;
+			}	
 		}
 	}
-	return -1; //if input is not correct
+	return -2; //if input is not correct
 }
 
 bool checkQuit(char prompt[]) {
@@ -92,10 +99,15 @@ int main() {
 		std::cin.getline(prompt, 255);
 		if(checkQuit(prompt)) {
 			done = true;
-			std::cout << "I WILL QUIT";
 		}
 		else {
-			std::cout << userInput(prompt) << std::endl;
+			double userReturn = userInput(prompt);
+			if (userReturn == -2) { //bad command entered
+				std::cout << showHelp() << std::endl;
+			}
+			else if (userReturn != -1) { //-1 if number out of bounds
+				std::cout << userReturn << std::endl;
+			}
 		}
 	}
 }
